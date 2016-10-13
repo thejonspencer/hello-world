81a9783cb4337e9250149c7fe84a3a89b7b26389
Jon <jon@thanspencer.com>
2016-10-12 21:25:38 -0400

    Commit the test
    committing

---
+++ src/test/java/jon/AppTest.java
@@ -0,0 +1,14 @@
+package jon;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void alwaysPass() {
+	Assert.assertTrue(true);
+    }
+
+}
