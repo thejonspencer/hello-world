3110c1261e433a5c22a7d2cc11614abf557f8b0f
Jon <jon@thanspencer.com>
2016-10-12 22:08:02 -0400

    adding some maven and spring garbage

--- src/main/java/jon/App.java
+++ src/main/java/jon/App.java
@@ -1,12 +1,21 @@
 package jon;
+
 import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(
+	    SpringApplicationBuilder application) {
+	return application.sources(App.class);
+    }
 
     public static void main(String[] args) {
 	ApplicationContext ctx = SpringApplication.run(App.class, args);
